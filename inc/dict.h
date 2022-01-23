493ed137f12d5f8e403537f936a1b99d2e546e17
alexinatra2 <a.holzknecht@gmx.de>
2022-01-23 14:00:51 +0100

    wrote headers for dict + entry

---
+++ inc/dict.h
@@ -0,0 +1,18 @@
+#include "entry.h"
+
+typedef enum { GERMAN, ENGLISH } language_e;
+typedef struct node node_t;
+typedef struct dict dict_t;
+
+dict_t createDict(language_e);
+int deleteDict(dict_t *d);
+
+int insertEntry(dict_t *d, entry_t *e);
+int removeEntry(dict_t *d, entry_t *e);
+
+int insertEntryByWords(dict_t *d, char *g, char *e);
+int removeEntryByWords(dict_t *d, char *g, char *e);
+
+int mergeDicts(dict_t *d1, dict_t *d2);
+
+void printDict(dict_t *d);
---
+++ inc/entry.h
@@ -0,0 +1,6 @@
+typedef struct entry entry_t;
+
+entry_t createEntry(char *g, char *e);
+int deleteEntry(entry_t *e);
+
+void printEntry(entry_t *e);
