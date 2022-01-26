c0868d7c96a0ecf72cb3ea3c2c0882792490001c
alexinatra2 <a.holzknecht@gmx.de>
2022-01-26 15:17:09 +0100

    removed some clutter sample code in main

--- main.c
+++ main.c
@@ -11,23 +11,7 @@ int main(int argc, char **argv) {
                      insertEntryStr(dict1, "nacht", "night") &&
                      insertEntryStr(dict1, "tag", "day") &&
                      insertEntryStr(dict1, "jahr", "year"));
-
   printDict(dict1);
-
-  dict_t *dict2 = createDict(GERMAN);
-  printf("%d\n", insertEntryStr(dict2, "hallo", "hello") &&
-                     insertEntryStr(dict2, "welt", "world") &&
-                     insertEntryStr(dict2, "mann", "man") &&
-                     insertEntryStr(dict2, "frau", "woman") &&
-                     insertEntryStr(dict2, "kind", "child"));
-
-  printDict(dict2);
-
-  mergeDicts(dict1, dict2);
-
-  printDict(dict1);
-
   deleteDict(dict1);
-  deleteDict(dict2);
   return 0;
 }
