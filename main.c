f178450e1b21963d029bf543e0300928d59c5afd
alexinatra2 <a.holzknecht@gmx.de>
2022-01-26 15:58:58 +0100

    added dict export and import functions to respective header file

--- main.c
+++ main.c
@@ -1,16 +1,13 @@
 #include "dict.h"
-#include "entry.h"
-#include "logging.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
   dict_t *dict1 = createDict(GERMAN);
-  printf("%d\n", insertEntryStr(dict1, "bauernhof", "farm") &&
-                     insertEntryStr(dict1, "ritter", "knight") &&
-                     insertEntryStr(dict1, "nacht", "night") &&
-                     insertEntryStr(dict1, "tag", "day") &&
-                     insertEntryStr(dict1, "jahr", "year"));
+  insertEntryStr(dict1, "bauernhof", "farm") &&
+      insertEntryStr(dict1, "ritter", "knight") &&
+      insertEntryStr(dict1, "nacht", "night") &&
+      insertEntryStr(dict1, "tag", "day") &&
+      insertEntryStr(dict1, "jahr", "year");
   printDict(dict1);
   deleteDict(dict1);
   return 0;
