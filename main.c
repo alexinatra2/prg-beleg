73d3a2c2337aa8af2a2ee98d9fa18e2fa36e8b6c
alexinatra2 <a.holzknecht@gmx.de>
2022-01-27 21:24:58 +0100

    added third cmd parameter to influence language direction of test dict

--- main.c
+++ main.c
@@ -1,37 +1,38 @@
 #include "dict.h"
 #include "dictIO.h"
+#include "entry.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #ifndef BUF_SIZE
 #define BUF_SIZE 128
 #endif // !BUF_SIZE
 
-dict_t *dict;
-
 int main(int argc, char **argv) {
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s <number>\n", argv[0]);
+  if (argc < 2 || argc > 3) {
+    fprintf(stderr, "usage: %s <amount_of_dict_entries_to_insert> [ger|eng]\n",
+            argv[0]);
     return EXIT_FAILURE;
   }
-  dict_t *dict = createDict(GERMAN);
 
-  char *german = malloc(BUF_SIZE);
-  char *english = malloc(BUF_SIZE);
-  if (german && english) {
+  language_e lang = argc == 3 && strcmp(argv[2], "eng") == 0 ? ENGLISH : GERMAN;
 
-    for (int i = 0; i < atoi(argv[1]); i++) {
-      printf("German word: ");
-      fgets(german, BUF_SIZE, stdin);
-      printf("English word: ");
-      fgets(english, BUF_SIZE, stdin);
-      insertEntryStr(dict, german, english);
-    }
+  dict_t *dict = createDict(lang);
+
+  char first[BUF_SIZE];
+  char second[BUF_SIZE];
+  for (int i = 0; i < atoi(argv[1]); i++) {
+    printf("%s word: ", lang == GERMAN ? "German" : "English");
+    fgets(first, BUF_SIZE, stdin);
+    printf("%s word: ", lang == ENGLISH ? "German" : "English");
+    fgets(second, BUF_SIZE, stdin);
+    lang == GERMAN ? insertEntryStr(dict, first, second)
+                   : insertEntryStr(dict, second, first);
   }
 
   printDict(dict);
-
-  deleteDict(dict);
 
   return EXIT_SUCCESS;
 }
