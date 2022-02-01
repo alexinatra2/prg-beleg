8242dd544d8edf480a6b5dd4d930a7789b1e54fd
alexinatra2 <a.holzknecht@gmx.de>
2022-02-01 12:13:32 +0100

    refactored not NULL check in main

--- main.c
+++ main.c
@@ -17,6 +17,9 @@
 #define BUF_SIZE 128
 #endif // !CONSTANTS
 
+void handleBaseDictNotPresent(char *program);
+int handleBaseDictNull(dict_t *d, language_e lang);
+
 int main(int argc, char **argv) {
   dict_t *german_base_dict;
   dict_t *german_additional_dict;
@@ -24,20 +27,16 @@ int main(int argc, char **argv) {
   dict_t *english_additional_dict;
 
   if (access(BASE_DICT_NAME, F_OK)) {
-    fprintf(stderr, "%s: file %s does not exist. Creating %s...\n", argv[0],
-            BASE_DICT_NAME, BASE_DICT_NAME);
-    char *touch_file_command = malloc(strlen(BASE_DICT_NAME) + 8);
-    if (touch_file_command) {
-      sprintf(touch_file_command, "touch %s", BASE_DICT_NAME);
-    }
-    if (system(touch_file_command)) {
-      fprintf(stderr, "Could not create file %s\n", BASE_DICT_NAME);
-    }
-    return EXIT_FAILURE;
+    handleBaseDictNotPresent(argv[0]);
   }
 
   german_base_dict = importDict(BASE_DICT_NAME, GERMAN);
   english_base_dict = importDict(BASE_DICT_NAME, ENGLISH);
+
+  if (handleBaseDictNull(german_base_dict, GERMAN) ||
+      handleBaseDictNull(english_base_dict, ENGLISH)) {
+    return EXIT_FAILURE;
+  }
 
   printDict(german_base_dict);
   printDict(english_base_dict);
@@ -48,4 +47,25 @@ int main(int argc, char **argv) {
   deleteDict(english_base_dict);
 
   return EXIT_SUCCESS;
+}
+
+void handleBaseDictNotPresent(char *program) {
+  fprintf(stderr, "%s: file %s does not exist. Creating %s...\n", program,
+          BASE_DICT_NAME, BASE_DICT_NAME);
+  char *touch_file_command = malloc(strlen(BASE_DICT_NAME) + 8);
+  if (touch_file_command) {
+    sprintf(touch_file_command, "touch %s", BASE_DICT_NAME);
+  }
+  if (system(touch_file_command)) {
+    fprintf(stderr, "Could not create file %s\n", BASE_DICT_NAME);
+  }
+  exit(EXIT_FAILURE);
+}
+
+int handleBaseDictNull(dict_t *d, language_e lang) {
+  if (d) {
+    return 0;
+  }
+  fprintf(stderr, "%s dict is NULL\n", lang == GERMAN ? "German" : "English");
+  return 1;
 }
