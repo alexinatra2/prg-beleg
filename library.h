d69ba48399a0c805eb71c1e7a9e1cf509b0d835b
alexinatra2 <a.holzknecht@gmx.de>
2022-02-05 19:41:16 +0100

    added header functions

--- library.h
+++ library.h
@@ -1,4 +1,18 @@
 #ifndef LIBRARY_H
 #define LIBRARY_H
 
+#include "medium.h"
+
+typedef struct lib lib_t;
+
+lib_t *createLib(void);
+int deleteLib(lib_t *lib);
+
+int insertMedium(lib_t *lib, medium_t *medium);
+int removeMedium(lib_t *lib, medium_t *medium);
+
+lib_t *lookup(lib_t *lib, filter_type_e filter_type, char *search_string);
+
+char *libToString(lib_t *lib);
+
 #endif // !LIBRARY_H
--- medium.h
+++ medium.h
@@ -1,4 +1,20 @@
 #ifndef MEDIUM_H
 #define MEDIUM_H
 
+typedef struct medium medium_t;
+
+typedef enum { BOOK, CD, DVD } medium_type_e;
+typedef enum { MEDIUM, BORROWER, ARTIST } filter_type_e;
+
+medium_t *createMedium(medium_type_e medium_type, char *medium_title, char *artist);
+int deleteMedium(medium_t *medium);
+
+int lendMediumTo(medium_t *medium, char *borrower);
+
+char *mediumToString(medium_t *medium);
+
+medium_type_e mediumTypeOf(medium_t *medium);
+char *borrowerOf(medium_t *medium);
+char *artistOf(medium_t *medium);
+
 #endif // !MEDIUM_H
